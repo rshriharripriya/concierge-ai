e5126f6aee4fa392b37fed6a012798f67a0ac1fc
rshriharripriya <rshriharripriya19@gmail.com>
2025-12-15 23:27:35 -0700

    refactor: Update `hover-border-gradient` to use `React.createElement` with improved event types and add `@types/three` dependency.

--- components/ui/hover-border-gradient.tsx
+++ components/ui/hover-border-gradient.tsx
@@ -55,18 +55,21 @@ export function HoverBorderGradient({
       return () => clearInterval(interval);
     }
   }, [hovered]);
-  return (
-    <Tag
-      onMouseEnter={(event: React.MouseEvent<HTMLDivElement>) => {
+
+  return React.createElement(
+    Tag,
+    {
+      onMouseEnter: (event: React.MouseEvent<HTMLElement>) => {
         setHovered(true);
-      }}
-      onMouseLeave={() => setHovered(false)}
-      className={cn(
+      },
+      onMouseLeave: () => setHovered(false),
+      className: cn(
         "relative flex rounded-full border  content-center bg-black/20 hover:bg-black/10 transition duration-500 dark:bg-white/20 items-center flex-col flex-nowrap gap-10 h-min justify-center overflow-visible p-px decoration-clone w-fit",
         containerClassName
-      )}
-      {...props}
-    >
+      ),
+      ...props,
+    },
+    <>
       <div
         className={cn(
           "w-auto text-white z-10 bg-black px-4 py-2 rounded-[inherit]",
@@ -94,6 +97,6 @@ export function HoverBorderGradient({
         transition={{ ease: "linear", duration: duration ?? 1 }}
       />
       <div className="bg-black absolute z-1 flex-none inset-[2px] rounded-[100px]" />
-    </Tag>
+    </>
   );
 }
